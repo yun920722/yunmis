41c97349b50d06b67a4c1131ce1eb155987f1b66
yun920722 <s1101686@gm.pu.edu.tw>
2022-11-23 09:13:03 +0800

    make it better

--- index.py
+++ index.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
 import firebase_admin
 from firebase_admin import credentials, firestore
 cred = credentials.Certificate("serviceAccountKey.json")
@@ -101,7 +104,24 @@ def movie():
         doc_ref.set(doc)
 
     return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate
-
+@app.route("/search", methods=["POST","GET"])
+def search():
+    if request.method == "POST":
+        MovieTitle = request.form["MovieTitle"]
+        info = ""     
+        collection_ref = db.collection("電影")
+        #docs = collection_ref.where("title","==", "夜鷹的單戀").get()
+        docs = collection_ref.order_by("showDate").get()  
+        for doc in docs:
+            if "：" in doc.to_dict()["title"]:
+                info += "片名：" + doc.to_dict()["title"] + "<br>" 
+                info += "海報：" + doc.to_dict()["picture"] + "<br>"
+                info += "影片介紹：" + doc.to_dict()["hyperlink"] + "<br>"
+                info += "片長：" + doc.to_dict()["showLength"] + " 分鐘<br>" 
+                info += "上映日期：" + doc.to_dict()["showDate"] + "<br><br>"           
+        return info
+    else:  
+        return render_template("input.html")
 
 
 
